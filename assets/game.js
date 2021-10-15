09c5b45ecc70600b458ca5bb474a3f8918109709
hanandadras <hanan.aabid@gmail.com>
2021-10-15 10:55:38 -0700

    executed javascript function

---
+++ assets/game.js
@@ -0,0 +1,15 @@
+// this creates a function named "fight"
+function fight() {
+    window.alert ("The fight has begun!");
+    window.prompt ("What is your robot's name?");
+    var playerName = window.prompt("What is your robot's name?");
+    console.log(playerName);
+    console.log("This logs a string, good for leaving yourself a message")
+    //this will do math and log 20
+    console.log(10+10)
+    //What is this?
+    console.log("Our robot's name is " + playername);
+    var name ="your name";
+    console.log(name);
+  }
+//fight()
