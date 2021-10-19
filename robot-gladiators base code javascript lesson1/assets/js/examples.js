d21af0bff2a01f95cd9cef845c56b2e39463c0a6
hanandadras <hanan.aabid@gmail.com>
2021-10-19 12:40:20 -0700

    for loop iteration failed try

---
+++ robot-gladiators base code javascript lesson1/assets/js/examples.js
@@ -0,0 +1,19 @@
+for(var i = 0; i < 3; i++) {
+    console.log("apple");
+}
+/ You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames[3]);
+console.log(enemyNames.length);
+
+
+*bottom of code replaced 
+// run fight function to start game
+var fight = function(enemyNames) {
+    ...//fight function statements
+  }
